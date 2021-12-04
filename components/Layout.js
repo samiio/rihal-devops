6b4c3f1693e056365ede5a1db61e1f02dc98fe8d
samiio <sami.ali.96@outlook.com>
2021-12-04 11:57:26 +0400

    fixed server side rendering issue

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,28 @@
 import { Container, Flex, VStack } from "@chakra-ui/react";
 import Nav from "./Nav";
 
-const Layout = ({ children }) => (
-  <div>
-    <Nav />
-    <Container maxWidth="container.lg" padding={0}>
-      <Flex height="calc(100vh - 50px)">
-        <VStack
-          width="full"
-          height="full"
-          padding={10}
-          spacing={10}
-          alignItems="center"
-          bg="gray.50"
-        >
-          {children}
-        </VStack>
-      </Flex>
-    </Container>
-  </div>
-);
+const Layout = ({ children }) => {
+  const SSR = typeof window === "undefined";
+
+  return (
+    <div>
+      <Nav />
+      <Container maxWidth="container.lg" padding={0}>
+        <Flex height="calc(100vh - 50px)">
+          <VStack
+            width="full"
+            height="full"
+            padding={10}
+            spacing={10}
+            alignItems="center"
+            bg="gray.50"
+          >
+            {!SSR ? children : null}
+          </VStack>
+        </Flex>
+      </Container>
+    </div>
+  );
+};
 
 export default Layout;
