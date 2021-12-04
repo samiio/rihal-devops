a0a509a807c9f92619d4e04a539c1a827ab5bd8d
samiio <sami.ali.96@outlook.com>
2021-12-04 20:05:34 +0400

    created modal

---
+++ components/StudentCreateModal.js
@@ -0,0 +1,60 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Input,
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+
+const StudentCreateModal = ({
+  title,
+  isOpen,
+  onOpen,
+  onClose,
+  name,
+  setName,
+  createClicked,
+}) => {
+  return (
+    <div>
+      <Button colorScheme="blue" w={8} h={8} onClick={onOpen} borderRadius={50}>
+        <AddIcon />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add {title}</ModalHeader>
+          <ModalCloseButton />
+
+          <form onSubmit={createClicked}>
+            <ModalBody>
+              <Input
+                placeholder="Name"
+                variant="filled"
+                mb={6}
+                type="text"
+                isRequired="true"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" type="submit">
+                Add
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+export default StudentCreateModal;
--- pages/students.js
+++ pages/students.js
@@ -1,9 +1,43 @@
 import Meta from "../components/Meta";
+import { studentController } from "rihal-devops-model";
+import { useDisclosure, Center } from "@chakra-ui/react";
+import { useState } from "react";
+import StudentCreateModal from "../components/BasicCreateModal";
 
-const StudentsPage = () => (
-  <div>
-    <Meta title="Students" />
-  </div>
-);
+const StudentsPage = () => {
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [countryId, setCountryId] = useState("");
+  const [classId, setClassId] = useState("");
+
+  const {
+    isOpen: isCreateOpen,
+    onOpen: onCreateOpen,
+    onClose: onCreateClose,
+  } = useDisclosure();
+
+  const handleCreateClick = (e) => {
+    e.preventDefault();
+    studentController.create(name);
+    onCreateClose();
+    setCountries(countryController.getAll().table);
+  };
+
+  return (
+    <div>
+      <Center>
+        <StudentCreateModal
+          isOpen={isCreateOpen}
+          onOpen={onCreateOpen}
+          onClose={onCreateClose}
+          name={name}
+          setName={setName}
+          createClicked={handleCreateClick}
+        />
+      </Center>
+      <Meta title="Students" />
+    </div>
+  );
+};
 
 export default StudentsPage;
